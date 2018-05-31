c85deaedcb6aca889c65ef2685926056a256d4b0
Jeffrey Getchell <getchell.jeffrey@gmail.com>
2018-05-30 20:26:58 -0400

    Dims & wts for giggles.

---
+++ 1_ java4pkg/js/main.js
@@ -0,0 +1,7 @@
+// Make it rain! This just happens to a first code project for dims & wts!
+
+var area = function(dims, length, width, height) {
+  console.log(dims + " = " + length * width * height + " cubic inches");
+}
+
+area("10 x 7 x 4 ", 10, 7, 4);
